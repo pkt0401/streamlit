f3fe88b92bbd70671b0d4acc8a6cfa52f6c219fb
pkt0401 <120307385+pkt0401@users.noreply.github.com>
2025-03-14 15:09:04 +0900

    Update test_stream.py

--- test_stream.py
+++ test_stream.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
+#import plotly.express as px
+#import plotly.graph_objects as go
 import datetime
 from datetime import timedelta
 import random
